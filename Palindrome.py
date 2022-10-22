d3e4a3c5c86bfd4fe9166ae068b858b9a58a662b
mmkhan1 <63598271+mmkhan1@users.noreply.github.com>
2022-10-22 14:01:02 +0400

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+#Palindrome
+   
+string=input(("Enter a string:"))
+if(string==string[-1::-1]):
+      print("The string is a palindrome")
+else:
+      print("Not a palindrome")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ Prime and Composit number.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+#Input a number and check if the number is prime or composite number
+
+n= int(input("Enter any number:"))
+if(n ==0 or n == 1):
+    printf(n,"Number is neither prime nor composite")
+elif n>1 :
+    for i in range(2,n):
+        if(n%i == 0):
+            print(n,"is not prime but composite number")
+            break
+    else:
+        print(n,"number is prime but not composite number")
+else :
+    print("Please enter positive number only ")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
